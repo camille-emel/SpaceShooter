a6e65ad3f2278f001e72de2258eb683557cfb24e
camille <emelcamille@gmail.com>
2024-12-08 16:38:52 +0100

    Projectile

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const shooter = document.getElementById("player-controlled-shooter");
+const mainPlayArea = document.getElementById("main-play-area");
 
 function moveUp() {
     let topPosition = window.getComputedStyle(shooter).getPropertyValue("top");
@@ -30,7 +31,36 @@ function letShipFly(event){
         moveDown();
     } else if(event.key === " "){
         event.preventDefault();
-        fireWeapon();
+        fireProjectile();
     }
+}
+
+function fireProjectile(){
+    let projectiles = createProjectileElement()
+    mainPlayArea.appendChild(projectiles);
+    let projectileSound = new Audio("audio/pew.m4a");
+    projectileSound.volume = 0.2;
+    projectileSound.play();
+    moveProjectile(projectiles);
+}
+function createProjectileElement(){
+    let xPos = parseInt(window.getComputedStyle(shooter).getPropertyValue("left"));
+    let yPos = parseInt(window.getComputedStyle(shooter).getPropertyValue("top"));
+    let newProjectile = document.createElement("img");
+    newProjectile.src ="images/projectile.png";
+    newProjectile.classList.add("projectile");
+    newProjectile.style.left = `${xPos}px`;
+    newProjectile.style.top = `${yPos - 10}px`;
+    return newProjectile;
+}
+function moveProjectile(projectiles){
+    let projectileInterval = setInterval(() => {
+        let xPos = parseInt(projectiles.style.left);
+        if(xPos === 340){
+            projectiles.remove();
+        }else {
+            projectiles.style.left = `${xPos + 4}px`;
+        }
+    }, 10)
 }
 window.addEventListener("keydown", letShipFly);
